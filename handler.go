3d47bf7f15fd4fd807ed95f54a40dfa1410de27c
ryuichi1208 <ryucrosskey@gmail.com>
2019-09-12 21:03:01 +0900

    first commit

--- handler.go
+++ handler.go
@@ -1,10 +1,22 @@
 package function
 
 import (
-	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Go serverless on Unubo Cloud. %s", string(req))
+	url := "http://google.co.jp"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	return string(byteArray)
+
+	// return fmt.Sprintf("Go serverless on Unubo Cloud. %s", string(req))
 }
