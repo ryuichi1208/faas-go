b2973beefd7ec8f27758bc8de0d152386b934107
ryuichi1208 <ryucrosskey@gmail.com>
2019-09-12 21:38:32 +0900

    first commit

--- handler.go
+++ handler.go
@@ -5,33 +5,25 @@ import (
 	"os/exec"
 )
 
+func ExecCmd() string {
+	var cmd [12]string
+	cmd[0] = "ps"
+	cmd[1] = "ls"
+	cmd[2] = "df"
+
+	ret := ""
+	for i := 0; i < len(cmd); i++ {
+		out, err := exec.Command(cmd[i]).Output()
+		if err != nil {
+			fmt.Println("Command Exec Error.")
+		}
+		ret = fmt.Sprintf("%s\n%s", ret, string(out))
+	}
+
+	return ret
+}
+
 // Handle a serverless request
 func Handle(req []byte) string {
-	// url := "http://google.co.jp"
-
-	// resp, err := http.Get(url)
-	// if err != nil {
-	// 	return ""
-	// }
-	// defer resp.Body.Close()
-
-	// _, _ := ioutil.ReadAll(resp.Body)
-
-	// return fmt.Sprintf("Go serverless on Unubo Cloud. %s", string(req))
-
-	out_ls, err := exec.Command("ls").Output()
-	if err != nil {
-		fmt.Println("Command Exec Error.")
-	}
-	out_ps, err := exec.Command("which", "pwd").Output()
-	if err != nil {
-		fmt.Println("Command Exec Error.")
-	}
-	out_pwd, err := exec.Command("pwd").Output()
-	if err != nil {
-		fmt.Println("Command Exec Error.")
-	}
-
-	// 実行したコマンドの結果を出力
-	return fmt.Sprintf("%s\n%s\n%s", string(out_ls), string(out_ps), string(out_pwd))
+	return ExecCmd()
 }
